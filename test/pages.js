7aa7e3a9982bca276f30656647542e1411414de4
calidion <caldiion@gmail.com>
2016-07-24 10:32:32 +0800

    enabled pages

---
+++ test/pages.js
@@ -0,0 +1,57 @@
+'use strict';
+var assert = require('assert');
+var path = require('path');
+var express = require('express');
+var parser = require('../lib/parser');
+var app = express();
+var config = parser(path.resolve(__dirname, './fixtures/config.yaml'));
+var settings = require('node-weixin-settings');
+var session = require('node-weixin-session');
+var pages = require('../lib/pages');
+
+pages(app, config, settings, session);
+
+var request = require('supertest');
+
+describe('pages', function () {
+  it('should get index', function (done) {
+    request(app)
+      .get('/')
+      .expect(200)
+      .end(function (err, res) {
+        assert(!err);
+        console.log(res.text);
+        done();
+      });
+  });
+  it('should get jssdk', function (done) {
+    request(app)
+      .get('/jssdk')
+      .expect(200)
+      .end(function (err, res) {
+        assert(!err);
+        console.log(res.text);
+        done();
+      });
+  });
+  it('should get oauth', function (done) {
+    request(app)
+      .get('/oauth')
+      .expect(200)
+      .end(function (err, res) {
+        assert(!err);
+        console.log(res.text);
+        done();
+      });
+  });
+  // it('should get pay', function (done) {
+  //   request(app)
+  //     .get('/pay')
+  //     .expect(200)
+  //     .end(function (err, res) {
+  //       assert(!err);
+  //       console.log(res.text);
+  //       done();
+  //     });
+  // });
+});
